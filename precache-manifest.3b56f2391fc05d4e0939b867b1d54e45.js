faf9d9d2e343c77aa66c8151a8405cb6d826b995
Michael Brauninger <mbrauninger@umass.edu>
2020-10-30 12:58:21 -0400

    Updates

---
+++ precache-manifest.3b56f2391fc05d4e0939b867b1d54e45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd17248c24da0589b7bf1908c1dbce9",
+    "url": "/new-sorting-visualizer/index.html"
+  },
+  {
+    "revision": "dd1d94bd598273c86aa8",
+    "url": "/new-sorting-visualizer/static/js/2.78894b98.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/new-sorting-visualizer/static/js/2.78894b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b6156de549d958acdc",
+    "url": "/new-sorting-visualizer/static/js/main.6caed35c.chunk.js"
+  },
+  {
+    "revision": "a7cf199db1c405778421",
+    "url": "/new-sorting-visualizer/static/js/runtime-main.d2fb2bb9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Bnew-sorting-visualizer%7D/precache-manifest.ace292fc1de0496156b9a4c7e600afe6.js"
+  "/new-sorting-visualizer/precache-manifest.3b56f2391fc05d4e0939b867b1d54e45.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bnew-sorting-visualizer%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-sorting-visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
